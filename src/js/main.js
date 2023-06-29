87d54f11219edde3c1856dd883ab56e821cd2da2
JulioRugolo <juliorugolo@gmail.com>
2023-06-29 20:08:53 -0300

    feat: implementing logic mobile/desktop navbar/toogle

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,57 @@
 const aButton = document.getElementById('loginButton');
 const menuBar = document.getElementById('menu-bar');
 const menuItems = menuBar.getElementsByTagName('p');
+const toggleMenu = document.getElementById('toggle-button');
 
 const user = JSON.parse(localStorage.getItem('user'));
-console.log(user);
+const isMobile = window.innerWidth < 768;
 
 if (user.loggedIn) {
   const registerLink = document.querySelector('a[href="./register.html"]');
   const loginLink = document.querySelector('a[href="./login.html"]');
   const navbar = document.querySelector('navbar');
-  
+  const togglePerson = document.getElementById('person');
+  const toggle = document.getElementById('toggle');
+
   if (registerLink) {
     navbar.removeChild(registerLink);
   }
-  
+
   if (loginLink) {
     const userName = user.name;
     const loginButton = loginLink.querySelector('p.navbar-item');
-    aButton.href = '#';
     loginButton.innerHTML = `Hola, <span class='username'>${userName}
     </span><img src='./img/down-outline.png'>`;
+    togglePerson.style.display = 'none';
+    toggle.style.display = 'flex';
+    toggleMenu.href = '#';
+    aButton.href = '#';
   }
 }
 
-aButton.addEventListener('click', () => {
-    menuBar.style.display = (menuBar.style.display === 'flex')
-      ? 'none'
-      : 'flex';
+if (isMobile) {
+  toggleMenu.addEventListener('click', () => {
+    menuBar.style.display = menuBar.style.display === 'flex' ? 'none' : 'flex';
   });
+} else {
+  aButton.addEventListener('click', () => {
+    menuBar.style.display = menuBar.style.display === 'flex' ? 'none' : 'flex';
+  });
+}
 
-  let selectedItem = null;
+let selectedItem = null;
   
-  const handleClick = (item) => {
-    if (selectedItem) {
-      selectedItem.style.color = ''; // Redefine a cor do item anterior
-    }
-    const buttonColor = item;
-    buttonColor.style.color = '#C5AF19'; // Aplica a cor ao novo item selecionado
-    selectedItem = buttonColor; // Armazena o novo item selecionado
-  };
-  
-  for (let i = 0; i < menuItems.length; i += 1) {
-    menuItems[i].addEventListener('click', () => {
-      handleClick(menuItems[i]);
-    });
+const handleClick = (item) => {
+  if (selectedItem) {
+      selectedItem.style.color = '';
   }
+  const buttonColor = item;
+  buttonColor.style.color = '#C5AF19';
+  selectedItem = buttonColor;
+  };
+
+  for (let i = 0; i < menuItems.length; i += 1) {
+  menuItems[i].addEventListener('click', () => {
+      handleClick(menuItems[i]);
+  });
+}
