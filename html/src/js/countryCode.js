647e71d018b2bfda2bfddc9e24b4a970d53c7429
JulioRugolo <juliorugolo@gmail.com>
2023-06-30 05:03:37 -0300

    fix: Changing countries API

--- html/src/js/countryCode.js
+++ html/src/js/countryCode.js
@@ -1,6 +1,4 @@
-// consumed API from:
-// https://restcountries.com/#endpoints-all
-const url = 'https://restcountries.com/v3.1/all';
+const url = 'https://api-paises.pages.dev/paises.json';
 
 const selectInput = document.getElementById('country');
 
@@ -8,19 +6,17 @@ const getCountryCode = async () => {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      // const findCountry = data.find((country) => country.name.common === 'United States');
-      // console.log(findCountry);
+
+      const dataNumber = Object.values(data).map(({ pais, ddi }) => {
+        const object = {
+          iddNumber: ddi,
+          fullName: pais, 
+        };
+        return object;
+      });
   
-      const dataNumber = data.map(({ idd, name }) => ({
-        fullName: name.common,
-        iddNumber: Array.isArray(idd.suffixes) ? idd.root : idd.root + idd.suffixes,
-      }));
-  
-      dataNumber
-        .filter(({ iddNumber }) => !Number.isNaN(iddNumber)) // filter out invalid numbers (api returns some invalid numbers)
-        .sort((a, b) => a.fullName.localeCompare(b.fullName))
-        .forEach(({ iddNumber, fullName }) => {
-          const option = new Option(`${fullName} ${iddNumber}`, iddNumber);
+      dataNumber.forEach(({ iddNumber, fullName }) => {
+          const option = new Option(`${fullName} +${iddNumber}`, iddNumber);
           selectInput.appendChild(option);
         });
     } catch (error) {
